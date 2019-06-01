751e4a582972939473add5b0854316c14ce2ccf6
SerhiiCho <serhiicho@protonmail.com>
2019-06-01 22:51:14 +0300

    Added custom styles for Photo report form

--- resources/js/components/Tabs/PhotoReport.vue
+++ resources/js/components/Tabs/PhotoReport.vue
@@ -1,26 +1,25 @@
 <template>
-    <form action="" enctype="multipart/form-data">
-        <table class="form-table">
-            <tbody>
-                <tr>
-                    <th><label for="sho-admin-name">Заголовок</label></th>
-                    <td><input type="text" id="sho-admin-name" name="name"></td>
-                </tr>
-                <tr>
-                    <th><label for="sho-admin-shortcode">Эльвира Шорткод</label></th>
-                    <td><input type="text" id="sho-admin-shorcode" name="shortcode"></td>
-                </tr>
-                <tr>
-                    <th><label for="sho-admin-image">Изображение</label></th>
-                    <td><input type="file" id="sho-admin-image" name="image"></td>
-                </tr>
-            </tbody>
-        </table>
-        <p class="submit">
-            <input type="submit" name="submit" class="button button-primary" value="Создать">
-            <a href="javascript:;" class="button">Автозаполнение</a>
-        </p>
-    </form>
+    <div class="sho-admin-form--wrap">
+        <form action="" enctype="multipart/form-data" class="sho-admin-form">
+            <div class="sho-admin-form__input">
+                <label for="sho-admin-name">Заголовок</label>
+                <input type="text" id="sho-admin-name" name="name">
+            </div>
+            <div class="sho-admin-form__input">
+                <label for="sho-admin-shortcode">Эльвира Шорткод</label>
+                <input type="text" id="sho-admin-shorcode" name="shortcode">
+            </div>
+            <div class="sho-admin-form__input">
+                <label for="sho-admin-image">Изображение</label>
+                <input type="file" id="sho-admin-image" name="image">
+            </div>
+
+            <div class="submit">
+                <input type="submit" name="submit" class="button button-primary" value="Создать">
+                <a href="javascript:;" class="button">Автозаполнение</a>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
