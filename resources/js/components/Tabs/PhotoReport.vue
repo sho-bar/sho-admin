88d737b6b975849a19edd61a57f4505083eae256
SerhiiCho <serhiicho@protonmail.com>
2019-06-02 16:49:58 +0300

    Added loading spinner

--- resources/js/components/Tabs/PhotoReport.vue
+++ resources/js/components/Tabs/PhotoReport.vue
@@ -3,19 +3,25 @@
         <form enctype="multipart/form-data" class="sho-admin-form" @submit.prevent="createReport($event)">
             <div class="sho-admin-form__input">
                 <label for="sho-admin-name">Заголовок</label>
-                <input type="text" id="sho-admin-name" name="name" value="11" required>
+                <input type="text" id="sho-admin-name" name="name" value="2019 в #ШО" required>
             </div>
             <div class="sho-admin-form__input">
                 <label for="sho-admin-shortcode">Эльвира Шорткод</label>
-                <input type="text" id="sho-admin-shorcode" name="shortcode" value="[some code here]" required>
+                <input type="text" id="sho-admin-shorcode" name="shortcode" value='[envira-gallery id=""]' required>
             </div>
             <div class="sho-admin-form__input">
                 <label for="sho-admin-image">Изображение</label>
-                <input type="file" id="sho-admin-image" name="image" >
+                <input type="file" id="sho-admin-image" name="image" required>
             </div>
             <div class="submit">
-                <input type="submit" class="button button-primary" value="Создать">
-                <a href="javascript:;" class="button">Автозаполнение</a>
+                <input type="submit"
+                    class="button button-primary"
+                    value="Создать"
+                    :disabled="loading">
+                <a href="javascript:;" class="button" :class="{disabled: loading}">
+                    Автозаполнение
+                </a>
+                <div class="sho-admin-spinner" v-if="loading"></div>
             </div>
         </form>
     </div>
@@ -26,10 +32,13 @@ import Request from '../../modules/Request'
 export default {
     data() {
         return {
+            loading: false,
         }
     },
     methods: {
         createReport(e) {
+            this.loading = true
+
             const formData = new FormData()
 
             formData.append('name', e.target.name.value)
@@ -38,8 +47,14 @@ export default {
             formData.append('action', 'add_photo_report')
 
             new Request(formData)
-                .then(res => console.log(res.data))
-                .catch(err => console.log(err))
+                .then(res => {
+                    this.loading = false
+                    console.log(res.data)
+                })
+                .catch(err => {
+                    this.loading = false
+                    console.log(err)
+                })
         },
     },
 }
