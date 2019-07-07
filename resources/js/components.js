f1906c16d50db3855fd4d31edf3521b5e859706e
SerhiiCho <serhiicho@protonmail.com>
2019-07-07 13:50:57 +0300

    Refactored components.js file structure

--- resources/js/components.js
+++ resources/js/components.js
@@ -1,7 +1,13 @@
-Vue.component('admin-btn', require('./components/AdminBtn'))
-Vue.component('modal', require('./components/Modal'))
-Vue.component('tabs', require('./components/Tabs'))
-Vue.component('tab', require('./components/Tab'))
+import PhotoReport from './components/Tabs/PhotoReport'
+import AdminBtn from './components/AdminBtn'
+import Modal from './components/Modal'
+import Tabs from './components/Tabs'
+import Tab from './components/Tab'
+
+Vue.component('admin-btn', AdminBtn)
+Vue.component('modal', Modal)
+Vue.component('tabs', Tabs)
+Vue.component('tab', Tab)
 
 // Tabs
-Vue.component('photo-report', require('./components/Tabs/PhotoReport'))
+Vue.component('photo-report', PhotoReport)
